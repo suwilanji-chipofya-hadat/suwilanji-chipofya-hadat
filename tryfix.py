e2475be093ade2c711b5864a43570761e38611fb
Suwilanji Chipofya <138398395+suwilanji-chipofya-hadat@users.noreply.github.com>
2023-07-23 12:54:45 +0000

    Update tryfix.py

--- tryfix.py
+++ tryfix.py
@@ -1,35 +1,15 @@
-def remove_extra_keys(list1, list2):
-    keys_set1 = set().union(*(d.keys() for d in list1))
-    keys_set2 = set().union(*(d.keys() for d in list2))
-    common_keys = keys_set1.intersection(keys_set2)
-
-    for item in list1:
-        item_keys = set(item.keys())
-        extra_keys = item_keys - common_keys
-        for key in extra_keys:
-            del item[key]
-
-    for item in list2:
-        item_keys = set(item.keys())
-        extra_keys = item_keys - common_keys
-        for key in extra_keys:
-            del item[key]
-
-    return list1, list2
-
-# Example lists of dictionaries
-list1 = [
-    {"a": 1, "b": 2, "c": 3},
-    {"b": 5, "c": 6, "d": 7}
-]
-
-list2 = [
-    {"a": 10, "b": 20, "c": 30},
-    {"b": 50, "c": 60, "e": 70}
-]
-
-list1, list2 = remove_extra_keys(list1, list2)
-print("List 1 after removing extra keys:")
-print(list1)
-print("List 2 after removing extra keys:")
-print(list2)
+bemba = []
+english = []
+for x in verses_bemba:
+  for y in verses_english:
+      for x_key, x_values in x.items():
+        for y_key, y_values in y.items():
+          b_text = ""
+          e_text = ""
+          if x_key == y_key:
+            for val in x_values:
+              b_text += val
+            for val in y_values:
+              e_text += val
+            bemba.append(b_text)
+            english.append(e_text)
